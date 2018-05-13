e4831bc6a4d17cd230cdf4f6c9b9c0198cad33d4
Aaayang <iamajser@163.com>
2018-05-13 21:45:04 +0800

    最简Promise

---
+++ p_01.js
@@ -0,0 +1,36 @@
+class Promise {
+    constructor(executor) {
+        this.status = 'pending';
+        this.value = undefined;
+        this.reason = undefined;
+
+        // new Promise中没有异步代码时一上来就会执行这里
+        let resolve = (data) => {
+            if (this.status === 'pending') {
+                this.status = 'resolved';
+                this.value = data;
+            }
+        }
+
+        let reject = (reason) => {
+            if (this.status === 'pending') {
+                this.status = 'rejected';
+                this.reason = reason;
+            }
+        }
+        executor(resolve, reject);
+    }
+    // 然后再执行这里的then
+    then(onFulFilled, onRejected) {
+        if (this.status === 'resolved') {
+            // onFulFilled是then中的第一个函数参数
+            onFulFilled(this.value);
+        }
+
+        if (this.status === 'rejected') {
+            onRejected(this.reason);
+        }
+    }
+}
+
+module.exports = Promise;
