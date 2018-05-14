961a827be48e86e41bd9a05d7d25f5116ce07ad3
Aaayang <iamajser@163.com>
2018-05-15 00:19:55 +0800

    链式调用

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-let Promise = require('./p_03');
+let Promise = require('./p_04');
 
 let p = new Promise((resolve, reject) => {
-    resolve(111);
+    resolve();
 });
 
+// 失败中返回的若是正常值(没有报错)仍会走下一个成功，即失败了也可以再成功
 
 p.then(data => {
-    return data;
+    return new Promise((resolve, reject) => {
+        // resolve(1);
+        resolve(new Promise((resolve ,reject) => {
+            resolve(111);
+        }));
+    });
 }, reason => {
-    
+
 }).then(data => {
     return data;
-}, reason => {
-    
 }).then(data => {
     console.log(data);
-}, reason => {
-    
 });
