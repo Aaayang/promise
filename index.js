948608030da032f321e0b2557fad37369cd1381b
Aaayang <iamajser@163.com>
2018-05-15 00:52:55 +0800

    promise中的then方法应该是异步调用的

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
-let Promise = require('./p_06');
+let Promise = require('./p_07');
 
 let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject('error');    
-    }, 1000);
+    resolve(1);
 });
 
-// 失败中返回的若是正常值(没有报错)仍会走下一个成功，即失败了也可以再成功
-
-/* p.then(data => {
-    return new Promise((resolve, reject) => {
-        // resolve(1);
-        resolve(new Promise((resolve ,reject) => {
-            resolve(111);
-        }));
-    });
-}, reason => {
-
-}).then(data => {
-    return data;
-}).then(data => {
-    console.log(data);
-}); */
-
-/* p.then(data => {
-    return new Promise((resolve, reject) => {
-        // resolve(1);
-        resolve(new Promise((resolve, reject) => {
-            resolve(111);
-        }));
-    });
-}, reason => {
-
-}).then(data => {
-    return data;
-}).then(data => {
-    console.log(data);
-}); */
-
-// then方法可以不传参，值的穿透
-/* p.then().then().then(null).then(null, err => {
-    console.log(err);
-}); */
-
-// 默认值
-/* p.then(y => y).then(y => y).then(y => y).then(data => {
-    console.log(data);
-}, err => {
-    console.log(err);
-}); */
-
+// promise中的then方法应该是异步调用的
 p.then().then().then(null).then(data => {
     console.log(data);
 }, err => {
-    console.log(err);
+    
 });
+
+console.log(2);
