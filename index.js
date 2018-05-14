81f848b7c2880d07627021b6b0f85af9eb4d4f18
Aaayang <iamajser@163.com>
2018-05-15 00:45:09 +0800

    值的穿透问题

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let Promise = require('./p_05');
+let Promise = require('./p_06');
 
 let p = new Promise((resolve, reject) => {
-    resolve();
+    setTimeout(() => {
+        reject('error');    
+    }, 1000);
 });
 
 // 失败中返回的若是正常值(没有报错)仍会走下一个成功，即失败了也可以再成功
@@ -21,7 +23,7 @@ let p = new Promise((resolve, reject) => {
     console.log(data);
 }); */
 
-p.then(data => {
+/* p.then(data => {
     return new Promise((resolve, reject) => {
         // resolve(1);
         resolve(new Promise((resolve, reject) => {
@@ -34,4 +36,22 @@ p.then(data => {
     return data;
 }).then(data => {
     console.log(data);
+}); */
+
+// then方法可以不传参，值的穿透
+/* p.then().then().then(null).then(null, err => {
+    console.log(err);
+}); */
+
+// 默认值
+/* p.then(y => y).then(y => y).then(y => y).then(data => {
+    console.log(data);
+}, err => {
+    console.log(err);
+}); */
+
+p.then().then().then(null).then(data => {
+    console.log(data);
+}, err => {
+    console.log(err);
 });
