544d86b6ed5fcc6e7d417d51679ca114fd3c50b7
Aaayang <iamajser@163.com>
2018-05-15 00:58:14 +0800

    promises-aplus-tests 测试

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-let Promise = require('./p_07');
+let Promise = require('./p_08');
 
 let p = new Promise((resolve, reject) => {
+    // executor执行的时候我们外面包了try catch，但是我们内部的代码是异步的就无法捕获了
+    // 需要给每个then中的方法都加一个try catch
+    /* setTimeout(() => {
+        reject('error');    
+    }, 1000); */
     resolve(1);
 });
 
-// promise中的then方法应该是异步调用的
+// 失败中返回的若是正常值(没有报错)仍会走下一个成功，即失败了也可以再成功
+
+/* p.then(data => {
+    return new Promise((resolve, reject) => {
+        // resolve(1);
+        resolve(new Promise((resolve ,reject) => {
+            resolve(111);
+        }));
+    });
+}, reason => {
+
+}).then(data => {
+    return data;
+}).then(data => {
+    console.log(data);
+}); */
+
+/* p.then(data => {
+    return new Promise((resolve, reject) => {
+        // resolve(1);
+        resolve(new Promise((resolve, reject) => {
+            resolve(111);
+        }));
+    });
+}, reason => {
+
+}).then(data => {
+    return data;
+}).then(data => {
+    console.log(data);
+}); */
+
+// then方法可以不传参，值的穿透
+/* p.then().then().then(null).then(null, err => {
+    console.log(err);
+}); */
+
+// 默认值
+/* p.then(y => y).then(y => y).then(y => y).then(data => {
+    console.log(data);
+}, err => {
+    console.log(err);
+}); */
+
+// promises-aplus-tests => 测试库
+
 p.then().then().then(null).then(data => {
     console.log(data);
 }, err => {
---
+++ p_08.js
@@ -0,0 +1,174 @@
+// 解析promise2和x之间的关系
+function resolvePromise(promise2, x, resolve, reject) {
+    // 怎么才能等于自己？
+    // 我们的promise和别人的promise可以进行交互
+
+    // 自己等自己既不会成功也不会失败
+    if (promise2 === x) {
+        return reject(new TypeError('循环引用'));
+    }
+
+    if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+        // 返回是promise是有可能又成功又失败的，例如resolve(1) reject(2)
+        // 防止既调成功又调失败
+        let called;
+        /* {
+            then: {}
+        } */
+
+        /* Object.defineProperty(x, 'then', {
+            get() {
+                throw new Error()
+            }
+        }) */
+        try {
+            // 防止通过Object.defineProperty取then时报错
+            let then = x.then;
+
+            if (typeof then === 'function') {
+                // 可以认为是promise了
+                then.call(x, y => {
+                    if (called) return;
+                    called = true;
+                    // resolve的结果依旧可能是个promise
+                    // resolve(y);
+                    // 如果y是promise就继续递归解析promise
+                    resolvePromise(promise2, y, resolve, reject)
+                }, err => {
+                    if (called) return;
+                    called = true;
+                    reject(err);
+                });
+            } else {
+                // then是一个普通对象
+                resolve(x);
+            }
+        } catch (e) {
+            if (called) return;
+            called = true;
+            reject(e);
+        }
+    } else {
+        // 直接走成功
+        resolve(x);
+    }
+}
+
+
+class Promise {
+    constructor(executor) {
+        this.status = 'pending';
+        this.value = undefined;
+        this.reason = undefined;
+        this.onFulFilledCallbacks = [];
+        this.onRejectedCallbacks = [];
+
+        // new Promise中没有异步代码时一上来就会执行这里
+        let resolve = (data) => {
+            if (this.status === 'pending') {
+                this.status = 'resolved';
+                this.value = data;
+
+                this.onFulFilledCallbacks.forEach((item) => item());
+            }
+        }
+
+        let reject = (reason) => {
+            if (this.status === 'pending') {
+                this.status = 'rejected';
+                this.reason = reason;
+            }
+
+            this.onRejectedCallbacks.forEach((item) => item());
+        }
+        try {
+            executor(resolve, reject);
+        } catch (e) {
+            reject(e);
+        }
+    }
+    // 然后再执行这里的then
+    then(onFulFilled, onRejected) {
+
+        onFulFilled = typeof onFulFilled === 'function' ? onFulFilled : y => y;
+        onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
+
+        // 上面onRejected throw err时，异步代码不会被捕获#1
+
+        let promise2;
+
+        if (this.status === 'resolved') {
+            // 调用then后会返回一个新的promise
+            promise2 = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    // 里面的代码可能是异步的#1
+                    try {
+                        // x指的是then中函数的返回值
+                        let x = onFulFilled(this.value);
+                        // 看其执行完是普通纸还是promise，如果是promise取他的结果作为
+                        // promise2成功的，如果返回一个普通值，直接作为promise2的成功的结果
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }    
+                });
+            });
+            // then返回一个新的promise
+        }
+
+        if (this.status === 'rejected') {
+            promise2 = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(this.reason);
+                        resolvePromise(promise2, x, resolve, reject);
+                        // x指的是then中函数的返回值
+                    } catch (e) {
+                        reject(e);
+                    }    
+                });
+            });
+            // then返回一个新的promise
+        }
+
+        if (this.status === 'pending') {
+            promise2 = new Promise((resolve, reject) => {
+                this.onFulFilledCallbacks.push(() => {
+                    setTimeout(() => {
+                        try {
+                            // 注意这里要统一用箭头函数，防止this出错
+                            let x = onFulFilled(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (e) {
+                            reject(e);
+                        }    
+                    });
+                });
+
+                this.onRejectedCallbacks.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onRejected(this.reason);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (e) {
+                            reject(e);
+                        }    
+                    });
+                });
+            });
+            // then返回一个新的promise
+        }
+        return promise2;
+    }
+}
+
+Promise.deferred = defer = function() {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+};
+
+module.exports = Promise;
